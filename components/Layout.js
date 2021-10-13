bb6013661c3153337f3c1209ea6493010b9c7554
PrincexGod <cvb.amaho@gmail.com>
2021-10-13 12:55:28 +0200

    NextLink

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Head from 'next/head'
 import {AppBar, Container, Toolbar, Typography} from "@mui/material";
 import useStyles from "../utils/styles";
+import NextLink from 'next/link'
+import {Link} from "@material-ui/core";
 
 export default function Layout({children}) {
     const classes = useStyles()
@@ -12,7 +14,18 @@ export default function Layout({children}) {
             </Head>
             <AppBar position="static" className={classes.navBar}>
                 <Toolbar>
-                    <Typography>Amazon Clone</Typography>
+                    <NextLink href="/" passHref>
+                        <Link><Typography className={classes.brand}>Amazon Clone</Typography></Link>
+                    </NextLink>
+                    <div className={classes.grow}></div>
+                    <div>
+                        <NextLink href='/cart' passHref>
+                            <Link>Cart</Link>
+                        </NextLink>
+                        <NextLink href='/login' passHref>
+                            <Link>Login</Link>
+                        </NextLink>
+                    </div>
                 </Toolbar>
             </AppBar>
             <Container className={classes.main}>
